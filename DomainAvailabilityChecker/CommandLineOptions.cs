df4e653ea045cfd09b31266c5545468b26206ed6
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2024-11-03 01:57:11 +0100

    readme

--- DomainAvailabilityChecker/CommandLineOptions.cs
+++ DomainAvailabilityChecker/CommandLineOptions.cs
@@ -7,6 +7,6 @@ public class CommandLineOptions
     [Option('i', "input", Required = true, HelpText = "The input file, containing a list of words separated by a newline.")]
     public string InputPath { get; set; }
 
-    [Option('o', "output", Required = true, HelpText = "The output file, containing the available domains found.")]
+    [Option('o', "output", Required = true, HelpText = "The output file, which will contain the available domains found.")]
     public string OutputPath { get; set; }
 }
